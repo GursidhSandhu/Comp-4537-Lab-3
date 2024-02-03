4aa7e618027eb8a38ba94592e5fb283abc4febdd
GursidhSandhu <gsandhu168@my.bcit.ca>
2024-02-03 15:22:24 -0800

    changed text to blue

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ const port = process.env.PORT || 8888;
 // listen on the port
 http.createServer(function(req,res) {
     res.writeHead(200,{'Content-Type': 'text/html'});
+
     let currentDate = date.date();
     let param = url.parse(req.url,true);
-    res.end(messages.part1 + param.query["name"]  + messages.part2 + currentDate);
+    let name = param.query["name"] || "Coder";
+    const color = 'style="color:blue;"';
+
+    let text = `<p ${color}>${messages.part1}${name}${messages.part2}${currentDate}</p>`;
+    res.end(text);
 }).listen(port);
 
